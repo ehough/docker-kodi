a98034940644300b1e41df21984003dea2d87363
Eric Hough <eric@tubepress.com>
2018-04-03 18:33:17 -0700

    copyright header reductions

--- docker/base/entrypoint.sh
+++ docker/base/entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-##########################################################################
-#
 # ehough/docker-kodi - Kodi in a Docker container
 #
 # https://github.com/ehough/docker-kodi
@@ -20,8 +18,6 @@
 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-##########################################################################
 
 
 ##########################################################################
